04f8ed0c9f66005682f42b8a5cbf94055731b9b7
Satyam19113 <Satyamkr1354@gmail.com>
2025-02-05 14:49:08 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import Header from './components/Header.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    this is a sample space
+    </>
+  )
+}
+
+export default App
